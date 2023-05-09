fba3dd9a2b7725b059faf1dfe18f0af37c9f77a3
Yahir Rios <125381043+Huevo131@users.noreply.github.com>
2023-05-09 14:23:02 -0600

    Add files via upload

---
+++ src/registrodeboda/RegistroDeBoda.java
@@ -0,0 +1,45 @@
+package registrodeboda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yahir
+ *
+ * While
+ * Programa RegistroDeBoda con un ciclo while y una terminación por petición del usuario.
+ * Este programa solicita los registros para regalos en una boda.
+ *
+ */
+
+public class RegistroDeBoda {
+    Scanner stdIn = new Scanner(System.in);
+    String registro = "";
+    char mas;
+    public void into()
+    {
+        System.out.print("¿Desea crear una lista de registro de bodas? (s/n): ");
+        mas = stdIn.nextLine().charAt(0);
+
+    }
+
+    public void  output()
+    {
+        while (mas == 's') {
+            System.out.print("Introduzca el nombre del artículo: ");
+            registro += stdIn.nextLine() + " - ";
+            System.out.print("Tienda: ");
+            registro += stdIn.nextLine() + "\n";
+            System.out.print("¿Algún otro artículo? (s/n): ");
+            mas = stdIn.nextLine().charAt(0);
+        } // fin del while
+        if (!registro.equals("")) {
+            System.out.println("\nRegistro de bodas:\n" + registro);
+        }
+    }
+    public static void main(String[] args) {
+        RegistroDeBoda x = new RegistroDeBoda();
+        x.into();
+        x.output();
+    } // fin del main
+} // fin de la clase RegistroDeBoda
